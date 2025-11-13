236bc8e7edb6eca106e2ca790e3237bff7a4fdf4
Altinisik nejdet <nejdet.altinisik@yahoo.fr>
2025-11-13 04:27:49 +0100

    Create Oarctolatentdarpaparamheuristiconne.h

---
+++ Oarctolatentdarpaparamheuristiconne.h
@@ -0,0 +1,7 @@
+
+
+//we can see truths with latent vars from oarc to darpa models
+//if we collect ui/ux models and .. models.
+//notice this is a trouble to say that.
+//so wed better start with topologies anomalies
+class checkanomaliesoncontractsandoptimizeonelineifplotplotalgorithm
